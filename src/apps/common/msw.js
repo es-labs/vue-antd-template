e657ecf5dc2defd989d1bbc4be71a9edde4dbf7b
ais-one <aaronjxz@gmail.com>
2023-09-25 02:14:09 +0800

    wip

--- src/apps/common/msw.js
+++ src/apps/common/msw.js
@@ -1,13 +1,12 @@
 // MSW - https://www.vuemastery.com/blog/mock-service-worker-api-mocking-for-vuejs-development-testing
 // run once: npx msw init public/
 // NOTE: if you touch MSW and it related code, need to do empty cache and hard reload
-import { worker } from '../web-sample/mocks/browser.js'
 
 export default async function prepare() {
   if (import.meta.env.MODE === 'mocked') {
-    // const { worker } = await import('../web-sample/mocks/browser')
+    const { worker } = await import('../web-sample/mocks/browser')
     console.log('MSW starting', worker)
-    return await worker.start({
+    return worker.start({
       onUnhandledRequest(request, print) {
         // Do not print warnings on unhandled requests to Sentry.
         if (request.url.hostname.includes('sentry.io')) return
